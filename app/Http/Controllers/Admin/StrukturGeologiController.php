3ecb0614a2f511a1534013b58b68b970678d4383
Meyzan16 <meyzan1605@gmail.com>
2022-11-07 22:46:31 +0700

    menghapus model yang tidak berguna

--- app/Http/Controllers/Admin/StrukturGeologiController.php
+++ app/Http/Controllers/Admin/StrukturGeologiController.php
@@ -4,15 +4,13 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
-use App\Models\struktur_geologi;
 
 class StrukturGeologiController extends Controller
 {
     
     public function index()
     {
-        $data = struktur_geologi::all();
-        return view('admin.main.struktur_geologi.index', compact('data'));
+        return view('admin.main.struktur_geologi.index');
     }
 
    
